aaa07cd189d02d72fcff6bf8ba48e131162d0ae6
britishpop <drmakc@gmail.com>
2018-11-05 18:31:17 +0300

    tabs first commit

---
+++ xhr/tabs/script.js
@@ -0,0 +1,24 @@
+const content = document.getElementById("content");
+const preloader = document.getElementById("preloader");
+const tabs = document.querySelectorAll("nav a");
+
+function parseData (data) {
+  console.log(data);
+}
+
+function showContent (event) {
+  event.preventDefault();
+  const destination = event.currentTarget.getAttribute("href");
+
+  const request = new XMLHttpRequest();
+  request.addEventListener("loadstart", () => preloader.classList.remove("hidden"));
+  request.addEventListener("loadend", () => preloader.classList.add("hidden"));
+  request.addEventListener("load", parseData(request.responseText));
+
+  request.open("GET", "file:///Users/britishpop/Documents/git/web_js/xhr/tabs/components/sms-tab.html", true);
+  request.send();
+}
+
+for (let tab of tabs) {
+  tab.addEventListener("click", showContent);
+}
