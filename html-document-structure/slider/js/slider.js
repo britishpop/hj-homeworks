81307b9f2042699288d4dfd86d3ccb0cdd6c1bf6
britishpop <drmakc@gmail.com>
2018-11-08 17:06:28 +0300

    slider finished

--- html-document-structure/slider/js/slider.js
+++ html-document-structure/slider/js/slider.js
@@ -1,46 +1,62 @@
 const container = document.querySelector("ul.slides");
-const slides = Array.from(document.getElementsByClassName("slide"));
-const buttons = Array.from(document.querySelectorAll(".slider-nav a"));
+const buttons = Array.from(container.parentElement.querySelectorAll(".slider-nav a"));
 
-const firstButton = buttons.find(button => button.dataset.action === "first");
-const prevButton = buttons.find(button => button.dataset.action === "prev");
-const nextButton = buttons.find(button => button.dataset.action === "next");
-const lastButton = buttons.find(button => button.dataset.action === "last");
 
-slides[0].classList.add("slide-current");
-buttons.forEach(button => slider(button));
+function slider(container) {
+  const firstCurrent = container.firstElementChild;
+  firstCurrent.classList.add("slide-current");
+  buttons.forEach(button => button.addEventListener("click", moveSlides));
+  updateDisabled(firstCurrent);
+}
 
-function slider (event) {
-  const activeSlide = container.querySelector(".slide-current");
 
-  if (!activeSlide.previousElementSibling) {
-    firstButton.classList.add("disabled");
-    prevButton.classList.add("disabled");
-  }
-  if (!activeSlide.nextElementSibling) {
-    nextButton.classList.add("disabled");
-    lastButton.classList.add("disabled");
+function moveSlides (event) {
+  const pressedButton = event.currentTarget;
+
+  if (pressedButton.classList.contains("disabled")) {
+    return;
   }
 
-  firstButton.addEventListener("click", moveSlides(false, true));
-  prevButton.addEventListener("click", moveSlides(false, false));
-  nextButton.addEventListener("click", moveSlides(true, false));
-  lastButton.addEventListener("click", moveSlides(true, true));
+  const currentSlide = container.querySelector(".slide-current");
+  let activatedSlide;
 
-  function moveSlides (isForward, isMax) {
-    activeSlide.classList.remove("slide-current");
-    if (isForward) {
-      if (isMax) {
-        slides[slides.length - 1].classList.add("slide-current");
-      } else {
-        activeSlide.nextElementSibling.classList.add("slide-current");
-      }
-    } else {
-      if (isMax) {
-        slides[0].classList.add("slide-current");
-      } else {
-        activeSlide.previousElementSibling.classList.add("slide-current");
-      }
-    }
+  switch (pressedButton.dataset.action) {
+    case "first":
+      activatedSlide = currentSlide.parentElement.firstElementChild;
+      break;
+    case "prev":
+      activatedSlide = currentSlide.previousElementSibling;
+      break;
+    case "next":
+      activatedSlide = currentSlide.nextElementSibling;
+      break;
+    case "last":
+      activatedSlide = currentSlide.parentElement.lastElementChild;
+      break;
+  }
+
+  currentSlide.classList.remove('slide-current');
+  activatedSlide.classList.add('slide-current');
+
+  updateDisabled (activatedSlide);
+}
+
+
+function updateDisabled (currentSlide) {
+  if (!currentSlide.previousElementSibling) {
+    buttons.find(button => button.dataset.action === "prev").classList.add("disabled");
+    buttons.find(button => button.dataset.action === "first").classList.add("disabled");
+  } else if (!currentSlide.nextElementSibling) {
+    buttons.find(button => button.dataset.action === "next").classList.add("disabled");
+    buttons.find(button => button.dataset.action === "last").classList.add("disabled");
+  }
+  if (currentSlide.previousElementSibling) {
+    buttons.find(button => button.dataset.action === "prev").classList.remove("disabled");
+    buttons.find(button => button.dataset.action === "first").classList.remove("disabled");
+  } else if (currentSlide.nextElementSibling) {
+    buttons.find(button => button.dataset.action === "next").classList.remove("disabled");
+    buttons.find(button => button.dataset.action === "last").classList.remove("disabled");
   }
 }
+
+slider(container);
