baf81dba757116c2a935d3b63112aed72ef55395
britishpop <drmakc@gmail.com>
2018-11-11 21:18:23 +0300

    sort finished

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  const currentTag = event.target;
+
+  if (currentTag.tagName === "TH") {
+    const sortDirection = currentTag.dataset.dir;
+    const table = document.getElementsByTagName("table")[0];
+    table.dataset.sortBy = currentTag.dataset.propName;
+
+    if (sortDirection > 0 || !sortDirection) {
+      currentTag.dataset.dir = -1;
+    } else if (sortDirection < 0) {
+      currentTag.dataset.dir = 1;
+    }
+
+    sortTable(table.dataset.sortBy, sortDirection);
+  }
 }
