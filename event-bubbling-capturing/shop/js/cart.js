c2a049d85d49cb80e305fc8b4f4096db86db2f6d
britishpop <drmakc@gmail.com>
2018-11-11 21:43:38 +0300

    shopcart finished

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,26 @@
 'use strict';
+
+showMore.addEventListener('click', reloadListeners);
+
+function reloadListeners () {
+  const addToCartButtons = document.getElementsByClassName("add-to-cart");
+  for (let button of addToCartButtons) {
+    button.addEventListener("click", middleman);
+  }
+}
+
+function middleman (event) {
+  event.preventDefault();
+  const currentItem = event.target;
+
+  if (currentItem.tagName !== "A") {
+    return;
+  }
+  const item = {
+    "title": currentItem.dataset.title,
+    "price": currentItem.dataset.price
+  };
+  addToCart(item);
+}
+
+reloadListeners()
