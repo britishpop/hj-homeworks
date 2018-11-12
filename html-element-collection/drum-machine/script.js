0a965b6799d1a56fe7b344f74feb06f23f822375
britishpop <drmakc@gmail.com>
2018-11-12 11:11:26 +0300

    drums now don't block audio

--- html-element-collection/drum-machine/script.js
+++ html-element-collection/drum-machine/script.js
@@ -1,10 +1,19 @@
-function play () {
-    let drum = document.getElementsByClassName(this.classList[1]);
-    drum[0].getElementsByTagName("audio")[0].play();
+function play (event) {
+  pauseAll();
+  const drum = document.getElementsByClassName(event.currentTarget.classList[1]);
+  drum[0].querySelector("audio").play();
+}
+
+function pauseAll () {
+  const sources = document.getElementsByTagName("audio");
+  for (let source of sources) {
+    source.pause();
+    source.currentTime = 0;
+  }
 }
 
 const drums = document.getElementsByClassName("drum-kit__drum");
 
 for (let drum of drums) {
-    drum.onclick = play;
+  drum.addEventListener("click", play);
 }
