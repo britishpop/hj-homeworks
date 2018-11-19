7d6c009eaf70abb1af6a9f94e26f5899d61f1b76
britishpop <drmakc@gmail.com>
2018-11-19 16:14:47 +0300

    bubbles finished

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+document.addEventListener('click', clickHandler);
+// connection.addEventListener('message', (event) => console.log(event));
+// connection.addEventListener('error', (event) => console.log(event));
+
+function clickHandler(event) {
+  const currentClick = {
+        'x': event.clientX,
+        'y': event.clientY
+      };
+  connection.send(JSON.stringify(currentClick));
+}
+
+showBubbles(connection);
