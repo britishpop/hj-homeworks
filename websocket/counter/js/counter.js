6c765fb1b7b757c5da83ccd4800502d87d7e1f4d
britishpop <drmakc@gmail.com>
2018-11-19 16:33:27 +0300

    counter finished

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const connectionCounter = document.querySelector('.counter'),
+      errorCounter = document.querySelector('output.errors'),
+      connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connection.addEventListener('message', parseMessage);
+// connection.addEventListener('error', (error) => console.log(error));
+document.addEventListener('beforeunload', () => connection.close(1000));
+
+function parseMessage(event) {
+  const data = JSON.parse(event.data);
+  connectionCounter.innerText = data.connections;
+  errorCounter.innerText = data.errors;
+}
