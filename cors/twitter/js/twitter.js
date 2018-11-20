8208d97651079d53d0574481d0b234ad9afa3561
britishpop <drmakc@gmail.com>
2018-11-20 10:04:13 +0300

    twitter finished

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const script = document.createElement('script');
+script.src = 'https://neto-api.herokuapp.com/twitter/jsonp';
+document.body.appendChild(script);
+
+const callback = function(data) {
+  const userpic = link(data.pic);
+  const wallpaper = link(data.wallpaper);
+
+  document.querySelector('[data-pic]').setAttribute('src', userpic);
+  document.querySelector('.bg').setAttribute('src', wallpaper);
+
+  document.querySelector('[data-username]').innerText = data.username;
+  document.querySelector('[data-description]').innerText = data.description;
+  document.querySelector('[data-tweets]').innerText = data.tweets;
+  document.querySelector('[data-followers]').innerText = data.followers;
+  document.querySelector('[data-following]').innerText =  data.following;
+}
+
+function link(link) {
+  return link.replace(/44889/,'');
+}
