7697cd512c16675c174afc5189397e83542f66bd
britishpop <drmakc@gmail.com>
2018-11-29 18:45:54 +0300

    store commit 1

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function createElement(node) {
+	if (typeof node === 'object') {
+		const fragment = document.createElement(node.name);
+		if (node.props) {
+			Object.keys(node.props).forEach(el => fragment.setAttribute(el, node.props[el]));
+		}
+		node.childs.forEach(el => {
+			fragment.appendChild(createElement(el));
+		});
+		return fragment;
+	}
+	if (typeof node === 'string') {
+		return document.createTextNode(node);
+	}
+}
