4df9e0cc1cca6d2014814193d93111ff372d4fae
britishpop <drmakc@gmail.com>
2018-12-02 09:48:19 +0300

    canvas draw commit 1

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const wss = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+editor.addEventListener('update', sendCanvas);
+
+function sendCanvas(e) {
+  e.canvas.toBlob(blob => wss.send(blob));
+}
