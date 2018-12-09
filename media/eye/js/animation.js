b04f26368ef86cb77724b14e447427a11ba19eb2
britishpop <drmakc@gmail.com>
2018-12-09 12:45:06 +0300

    eye commit 1

--- media/eye/js/animation.js
+++ media/eye/js/animation.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const pupil = document.querySelector('.big-book__pupil');
+const eye = document.querySelector('.big-book__eye');
+
+let mouseX = 0;
+let mouseY = 0;
+
+const eyeX = (eye.getBoundingClientRect().left + eye.getBoundingClientRect().right) / 2 + pageXOffset;
+const eyeY = (eye.getBoundingClientRect().top + eye.getBoundingClientRect().bottom) / 2 + pageYOffset;
+
+const leftEye = eyeX;
+const upEye = eyeY;
+
+const rightEye = document.body.scrollWidth - eyeX;
+const downEye = document.body.scrollHeight - eyeY;
+
+document.addEventListener('mousemove', event => {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+});
+
+
+function tick() {
+
+    const pupilX = mouseX < eyeX ? 30 * (mouseX - eyeX) / leftEye : 30 * (mouseX - eyeX) / rightEye;
+    const pupilY = mouseY < eyeY ? 30 * (mouseY - eyeY) / upEye : 30 * (mouseY - eyeY) / downEye;
+    const sizeChanger = Math.max(Math.abs(pupilX), Math.abs(pupilY));
+
+    const pupilSize = 3 - (sizeChanger / 15);
+
+    pupil.style.setProperty('--pupil-x', `${pupilX}px`);
+    pupil.style.setProperty('--pupil-y', `${pupilY}px`);
+    pupil.style.setProperty('--pupil-size', pupilSize);
+
+    window.requestAnimationFrame(tick);
+}
+
+tick();
